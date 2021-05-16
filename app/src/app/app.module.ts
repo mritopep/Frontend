1c17ffbec685ef71b20f945fdf719027d6e1fe0b
ashia <ashiajoseph@gmail.com>
2021-05-16 16:42:16 +0530

    Progress spinner color changed and centered,  Image size fitting

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -23,7 +23,7 @@ import {MatSliderModule} from '@angular/material/slider';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 
-const config: SocketIoConfig = { url: "http://f973505d5a5d.ngrok.io" , options: {} };
+const config: SocketIoConfig = { url: "http://d74020f41ae0.ngrok.io" , options: {} };
 import { HttpClientModule } from '@angular/common/http';
 
 
